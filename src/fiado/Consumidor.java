a870391569ae8a83b41597390a1ab3106429f45a
bosi <48396676+mabosh@users.noreply.github.com>
2021-04-12 23:53:22 -0300

    Aula 05

--- src/fiado/Consumidor.java
+++ src/fiado/Consumidor.java
@@ -3,7 +3,37 @@ package fiado;
 public class Consumidor {
 	
 	// Atributos
-	String nome;
-	int[] fiados;
+	//  PACKAGE - PUBLIC - PROTECTED - PRIVATE
+	private String nome;
+	private int[] fiados;
+	private int posicao = 0;
+	
+	
+	public Consumidor(String nome) {
+		this.nome = nome;
+		this.fiados = new int[10];
+	}
+	
+		// GETTER E SETTER
+		public String getNome( ) {
+			return this.nome;
+		}
+		
+		public void setNome(String nome) {
+			this.nome = nome;
+		}		
+		
+		public void registrarFiado(int valor) { 
+			this.fiados[posicao] = valor;
+			posicao++;
+		}
+		
+		public int getFiado() {
+			int total = 0;
+			for (int fiado: fiados) {
+				total += fiado;
+			}
+			return total;
+		}
 
 }
--- src/fiado/Main.java
+++ src/fiado/Main.java
@@ -5,23 +5,25 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-				
-		Consumidor consumidor = new Consumidor();
-		consumidor.fiados = new int[10];
 		
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Informe o nome do cliente: ");
-		consumidor.nome = scanner.nextLine();
-		System.out.print("Infome o valor da compra: ");
-		consumidor.fiados[0] = Integer.valueOf(scanner.nextLine());
 		
-		int total = somaWhile(consumidor.fiados);
+		String nome = scanner.nextLine();
+		Consumidor consumidor = new Consumidor(nome);
+		
+		System.out.print("Infome o valor da compra: ");
+		int fiado = Integer.valueOf(scanner.nextLine());
+		consumidor.registrarFiado(fiado);
+		
+		int total = consumidor.getFiado();
+//		int total = somaWhile(consumidor.fiados);
 		
 		if (total > 100) {
-			System.out.println("Cliente " + consumidor.nome + " deve: " + total);
-		} //else {
-			//System.out.println("Cliente " + consumidor.nome + " deve: " + total);
-		//}
+			System.out.println("Cliente " + consumidor.getNome() + " deve: " + total);
+		} else {
+			System.out.println("Cliente " + consumidor.getNome() + " deve: " + total);
+		}
 	}
 	
 	
