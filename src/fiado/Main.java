cf6c242ce55eacd00d4a782879973818419b30b4
bosi <48396676+mabosh@users.noreply.github.com>
2021-04-12 22:08:07 -0300

    commit

---
+++ src/fiado/Main.java
@@ -0,0 +1,61 @@
+package fiado;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Conjunto finito e ordenado
+		int[] fiados = new int[10];
+//		fiados[0] = 12;
+//		fiados[3] = 87;
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Informe o nome do cliente: ");
+		String nome = scanner.nextLine();
+		System.out.print("Infome o valor da compra: ");
+		fiados[0] = Integer.valueOf(scanner.nextLine());
+		
+		int total = somaWhile(fiados);
+		
+		System.out.println("O Cliente " + nome + " deve: " + total);
+		
+	}
+
+	
+	
+	public static int somaWhile(int[] fiados) {
+		int i = 0;
+		int total = 0;
+		while (i < fiados.length) {
+			total += fiados[i];
+			i++;
+		}
+		return total;
+
+	}
+
+	public static int somaFor(int[] fiados) {
+		int total = 0;
+		for (int i = 0; i < fiados.length; i++) {
+			total += fiados[i];
+
+		}
+		
+		return total;
+		
+		}
+	
+	public static int somaForeach(int[] fiados) {
+		int total = 0;
+		for (int fiado : fiados) {
+			total += fiado;				
+		}
+		
+		return total;
+	
+	}
+
+}
